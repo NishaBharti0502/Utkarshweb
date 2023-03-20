7ebd9ae86f5bdba7ab3b3cee05099fe08df036c4
Nisha Bharti <119736262+NishaBharti0502@users.noreply.github.com>
2023-03-21 00:00:32 +0530

    I changed line 58 pass because that is my real pass of the mail

--- signup.php
+++ signup.php
@@ -55,7 +55,7 @@ else{
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'nisha0502bharti@gmail.com';
-    $mail->Password = 'nishaiitian';
+    $mail->Password = 'nisha';
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
     $mail->setFrom('nisha0502bharti@gmail.com', 'Nisha');
